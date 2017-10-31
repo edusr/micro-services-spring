d93bf0152c6f30cd8b07c33ab311ce7ab3ddad26
eduardosr <eduardosr@peixoto.com.br>
2017-10-31 16:15:44 -0200

    first commit

---
+++ abobora/src/test/java/trup/de/maluco/abobora/AboboraApplicationTests.java
@@ -0,0 +1,16 @@
+package trup.de.maluco.abobora;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AboboraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
